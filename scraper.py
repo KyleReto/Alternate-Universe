2dca7ed5242be65f3c4cd54955cb7f04d6f9ac5a
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-01-19 02:05:38 -0500

    Finished scraper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import discord as ds
+import json
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,15 +13,40 @@ client = ds.Client()
 async def on_ready():
     print(f'{client.user} connected successfully')
     guild = await client.fetch_guild(GUILD_ID)
+    targets = TARGET_CHANNELS.split(' ')
+    targets = [int(i) for i in targets]
+
     channels = []
-    print(TARGET_CHANNELS)
-    for channelABC in await guild.fetch_channels():
-        print('test')
+    for channel_abc in await guild.fetch_channels():
+        if (channel_abc.id in targets):
+            channels.append(channel_abc)
+
     print(f'Guild: {guild}')
+
     for channel in channels:
+        file = open(channel.name + ".txt", "w", encoding='utf-8')
         print(f'Scraping channel: {channel}')
-        async for message in channel.history(limit=200):
-            print(f'{message.content}')
+        async for message in channel.history(limit=None):
+            encoded = f'[{message.author.name};{message.clean_content}'
+            if message.reference and message.reference.resolved and not (isinstance(message.reference.resolved, ds.DeletedReferencedMessage)):
+                encoded += f';{message.reference.resolved.author};{message.reference.resolved.clean_content}'
+            file.write(encoded + "]\n")
+        file.close()
+    print("Finished.")
+    sys.exit(0)
+
+# Format of a proper encode is as follows:
+# [author;message;referenceAuthor;referenceContent(missing if absent)] (Newline between entries)
+def encode(s):
+    replace_map = (
+            ("]", '&(rb)'),
+            ('[', '&(lb)'),
+            (';', '&(sc)'),
+            ('\n', '&(nl)')
+        )
+    for mapping in replace_map:
+        s = s.replace(mapping[0], mapping[1])
+    return s
 
 
 client.run(TOKEN)
