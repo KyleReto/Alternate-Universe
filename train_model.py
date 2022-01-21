579e6a5dfafc40f46a079a0fdc2dafa86fd62b4f
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-01-20 21:18:26 -0500

    Added model finetuning

---
+++ train_model.py
@@ -0,0 +1,26 @@
+import gpt_2_simple as gpt2
+import os
+
+model_name = "124M"
+if not os.path.isdir(os.path.join("models", model_name)):
+	print(f"Downloading {model_name} model...")
+	gpt2.download_gpt2(model_name=model_name)
+
+file_name = "scrapes/completeScrape.txt"
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              file_name,
+              model_name=model_name,
+              sample_every=20,
+              overwrite=True,
+              steps=1000)   # steps is max number of training steps
+
+#try .generate_to_file()
+gpt2.generate(sess,
+              length=500,
+              temperature=0.7,
+              prefix="",
+              nsamples=5,
+              batch_size=5
+              )
