ede2f823bbc2687df42dff97564c471ba1e870c0
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-01-21 21:42:31 -0500

    Added the ability to change models

--- train_model.py
+++ train_model.py
@@ -1,28 +1,23 @@
 import gpt_2_simple as gpt2
 import os
+from dotenv import load_dotenv
+load_dotenv()
+MODEL_NAME = os.getenv('GPT_2_MODEL')
 
-model_name = "124M"
-if not os.path.isdir(os.path.join("models", model_name)):
-	print(f"Downloading {model_name} model...")
-	gpt2.download_gpt2(model_name=model_name)
+if not os.path.isdir(os.path.join("models", MODEL_NAME)):
+	print(f"Downloading {MODEL_NAME} model...")
+	gpt2.download_gpt2(model_name=MODEL_NAME)
 
 file_name = "scrapes/completeScrape.txt"
 
 sess = gpt2.start_tf_sess()
+# Due to encoding options not being allowed in gpt-2-simple, this currently does not support certain special characters.
+# This may result in occasional crashes.
 gpt2.finetune(sess,
               file_name,
-              model_name=model_name,
+              model_name=MODEL_NAME,
               sample_every=20,
-              save_every=100,
+              save_every=20,
               restore_from='latest',
               overwrite=True,
               steps=1000)   # steps is max number of training steps
-
-#try .generate_to_file()
-gpt2.generate(sess,
-              length=500,
-              temperature=0.7,
-              prefix="",
-              nsamples=5,
-              batch_size=5
-              )
