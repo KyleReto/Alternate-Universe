299603dda6e89199c7827cab72e1ceaa20625141
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-10-20 02:14:33 -0400

    Fixed some bugs, updated partial examples

--- prepare.py
+++ prepare.py
@@ -1,10 +1,14 @@
 from doctest import SKIP
+from lib2to3.pgen2.token import PERCENT
 import os
 import sys
+from tkinter.tix import WINDOW
 from dotenv import load_dotenv
 load_dotenv()
 
-MAX_EXAMPLES = os.getenv('MAX_EXAMPLES')
+WINDOW_SIZE = os.getenv('WINDOW_SIZE')
+PERCENT_READ = os.getenv('PERCENT_READ')
+END_PROMPT = os.getenv('END_PROMPT')
 
 # Replaces any unsafe characters for jsonl
 def encode(s):
@@ -12,7 +16,7 @@ def encode(s):
             ("\"", '&(quot)'),
             ("\\", '&(bksl)'),
             ("	", '&(tab)'),
-            # This is symptomatic of my approach of blacklisting instead of whitelisting. I'll keep this in mind for next time...
+            # A specific breaking character found in my discord's history. This shows a flaw in the code structure here, honestly.
             ("", ''),
         )
     for mapping in replace_map:
@@ -22,38 +26,37 @@ def encode(s):
 # Prepare data to be imported to gpt-3
 prepared = open("scrapes/prepared.jsonl", "w", encoding='utf8')
 complete_scrapes = open("scrapes/completeScrape.txt", "r", encoding='utf8')
-n = 6
+n = 0
 num_examples = 0
 lines = complete_scrapes.readlines()
 enumerated_lines = enumerate(lines)
+# This loop is a little complex: The idea is to take a rolling window of quotes and convert them to jsonl prompt/completion format.
+# The quotes range from 1 prompt 1 completion to n prompts and 1 completion.
 for i, line in enumerated_lines:
     str = '{\"prompt\":\"'
-    # This approach skews data toward overprioritizing the first in a chain of messages. 
-    # TODO: A rolling window may be more fitting for this application (needs testing).
-    n = (n%6)+1 # ranges from 1 to 6
+    n = (n%(int(WINDOW_SIZE)))+1 # ranges from 1 to max
     try:
-        for j in range(1, n+1): # ranges from 1 to 6 as well
+        # Note that we're reading a few lines ahead here
+        for j in range(0, n): # ranges from 0 to max-1
             str += encode(lines[i+j].rstrip()) + '\\n'
+        str += END_PROMPT + '\", \"completion\":\" '
+        str += encode(lines[i+j+1].rstrip()) + '\\n\"}\n'
+        # Find out how many lines we have to skip, if any, to read the right percentage.
+        try:
+            ignore_lines = int((n+1) * (1-float(PERCENT_READ))/float(PERCENT_READ))
+        except ZeroDivisionError:
+            ignore_lines = 0
+        # Skip over any additional lines we've read, plus any we'd like to skip
+        for j in range(ignore_lines + n):
             next(enumerated_lines)
-        str += '\", \"completion\":\"'
-        str += encode(lines[i+n+1].rstrip()) + '\\n\"}\n'
-    except IndexError:
+    except (IndexError, StopIteration):
         # If we're out of lines to read, don't write anything and the program safely. I realize that this is hacky, but it's the most robust and dev-time-efficient way to solve this problem.
         complete_scrapes.close()
         prepared.close()
         exit()
-    # TODO: Rewite to skip over lines instead of having a hard limit, as this skews toward older messages in alphabetically higher channels.
-    if num_examples >= int(MAX_EXAMPLES):
-        # If we're at or over our limit, exit cleanly.
-        complete_scrapes.close()
-        prepared.close()
-        exit()
-    next(enumerated_lines)
     prepared.write(str)
-    num_examples += 1
 
-# Notes:
-
+# End structure is as follows:
 # {"prompt":"""
 # Author1: <message1>\n
 # Author2: <response1>\n
@@ -61,9 +64,3 @@ for i, line in enumerated_lines:
 # "completion": """
 # GenAuthor: <response2>\n
 #  """}
-
-# {"prompt":'[given prompt]', "completion": '[response]'}
-
-# List the ending token as the stop sequence (stop="\n")
-# prompt + completion < 1500 words
-# Lower learning rate and 1-2 epochs tend to be ideal.
